5e5b2afad57417e2195283e3fbbafd7904637625
Michael Stuhlman <msstuhlman@gmail.com>
2019-11-07 22:34:38 -0600

    Guaranteed uniqueness

--- src/main/java/ArtImage.java
+++ src/main/java/ArtImage.java
@@ -1,21 +1,26 @@
 import java.awt.image.BufferedImage;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ArtImage {
 
   private BufferedImage bufferedImage;
-  private int width;
-  private int height;
+  private float width;
+  private float height;
   private int[][] image;
   private int pixelMultiplier = 1;
+  private Set<Integer> uniqueColors;
 
   ArtImage(int width, int height) {
     this.width = width;
     this.height = height;
     image = new int[width][height];
     bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    uniqueColors = new HashSet<Integer>();
   }
 
   public void generateInitialImage() {
+    uniqueColors = new HashSet<Integer>();
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
         int r = ((int) (Math.random() * 255));
@@ -23,6 +28,7 @@ public class ArtImage {
         int b = ((int) (Math.random() * 255));
         int p = (r << 16) | (g << 8) | b;
         image[x][y] = p;
+        uniqueColors.add(p);
       }
     }
   }
@@ -49,6 +55,10 @@ public class ArtImage {
     this.pixelMultiplier = pixelMultiplier;
     bufferedImage =
         new BufferedImage(
-            width * pixelMultiplier, height * pixelMultiplier, BufferedImage.TYPE_INT_RGB);
+                (int)width * pixelMultiplier, (int)height * pixelMultiplier, BufferedImage.TYPE_INT_RGB);
+  }
+
+  float getUniqueness() {
+    return uniqueColors.size() / (width * height);
   }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,27 @@ import java.util.Date;
 
 public class Main {
   public static void main(String[] args) {
-    //GR: 1.61803398875
-    ArtImage artImage = new ArtImage(162, 100);
-    artImage.generateInitialImage();
+    // GR: 1.61803398875
+    ArtImage artImage = new ArtImage(100, 100);
     artImage.setPixelMultiplier(16);
+    int genCount = 0;
+
+    for(;;) {
+        genCount++;
+        artImage.generateInitialImage();
+        if (genCount % 100 == 0) System.out.println(genCount + " - uniqueness - " + artImage.getUniqueness());
+        if (!(artImage.getUniqueness() < 1)) {
+            System.out.println(genCount + " - uniqueness - " + artImage.getUniqueness());
+            break;
+        }
+    }
 
     try {
       String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-      ImageIO.write(artImage.getBufferedImage(), "png", new File("images/test" + timeStamp + ".png"));
+      ImageIO.write(
+          artImage.getBufferedImage(),
+          "png",
+          new File("ArtImage_" + timeStamp + ".png"));
     } catch (IOException e) {
       System.out.println("Error: " + e);
     }
