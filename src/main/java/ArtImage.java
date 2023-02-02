bca6320fac28de65d30298a8f3d9971b111fcb41
Michael Stuhlman <mstuhlman@finishline.com>
2023-02-01 23:18:02 -0600

    New code and more images

--- src/main/java/ArtImage.java
+++ src/main/java/ArtImage.java
@@ -5,102 +5,101 @@ import java.util.Collections;
 import java.util.List;
 
 public class ArtImage {
+    private final int WHITE_RGB = 16777215;
+    private BufferedImage bufferedImage;
+    private float width;
+    private float height;
+    private int red;
+    private int green;
+    private int blue;
+    private int[][] image;
+    private int pixelMultiplier = 1;
+    private List<Integer> uniqueColors;
+    private List<Integer> usedColors;
+    private List<Integer> knownPrimes;
+    private List<Integer> knownComposites;
 
-  private BufferedImage bufferedImage;
-  private float width;
-  private float height;
-  private int red;
-  private int green;
-  private int blue;
-  private int[][] image;
-  private int pixelMultiplier = 1;
-  private List<Integer> uniqueColors;
-  private List<Integer> usedColors;
-  private List<Integer> knownPrimes;
-  private List<Integer> knownComposites;
-
-  ArtImage(int width, int height, int red, int green, int blue, List<Integer> knownPrimes, List<Integer> knownComposites) {
-    this.width = width;
-    this.height = height;
-    this.red = red;
-    this.green = green;
-    this.blue = blue;
-    image = new int[width][height];
-    bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    uniqueColors = new ArrayList<Integer>();
-    usedColors = new ArrayList<Integer>();
-    this.knownPrimes = knownPrimes;
-    this.knownComposites = knownComposites;
-  }
-
-  public void generateImage() {
-
-    uniqueColors = new ArrayList<Integer>();
-    for (int r = 0; r < red; r++) {
-      for (int g = 0; g < green; g++) {
-        for (int b = 0; b < blue; b++) {
-          int p = (r << 16) | (g << 8) | b;
-          uniqueColors.add(p);
-        }
-      }
+    ArtImage(int width, int height, int red, int green, int blue, List<Integer> knownPrimes, List<Integer> knownComposites) {
+        this.width = width;
+        this.height = height;
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        image = new int[width][height];
+        bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        uniqueColors = new ArrayList<Integer>();
+        usedColors = new ArrayList<Integer>();
+        this.knownPrimes = knownPrimes;
+        this.knownComposites = knownComposites;
     }
-    Collections.shuffle(uniqueColors);
-    int INDEX = 1;
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        if (usedColors.contains(INDEX)) {
-          System.out.println("Collision! Continuing...");
-          INDEX++;
-          continue;
-        }
-        if (knownPrimes.contains(INDEX)) {
-          image[x][y] = 16777215;
-        } else {
-          if (knownComposites.contains(INDEX)) {
-            image[x][y] = uniqueColors.get(INDEX);
-            usedColors.add(INDEX);
-          } else {
-            AKS prime = new AKS(BigInteger.valueOf(INDEX));
-            boolean isPrime = prime.checkIsPrime(BigInteger.valueOf(INDEX));
-            if (isPrime) {
-              knownPrimes.add(INDEX);
-              image[x][y] = 16777215;
-            } else {
-              knownComposites.add(INDEX);
-              image[x][y] = uniqueColors.get(INDEX);
-              usedColors.add(INDEX);
+
+    public void generateImage() {
+        uniqueColors = new ArrayList<Integer>();
+        for (int r = 0; r < red; r++) {
+            for (int g = 0; g < green; g++) {
+                for (int b = 0; b < blue; b++) {
+                    int p = (r << 16) | (g << 8) | b;
+                    uniqueColors.add(p);
+                }
             }
-          }
         }
-        INDEX++;
-      }
+        Collections.shuffle(uniqueColors);
+        int INDEX = 1;
+        AKS prime = new AKS();
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (usedColors.contains(INDEX)) {
+                    System.out.println("Collision! Continuing...");
+                    INDEX++;
+                    continue;
+                }
+                if (knownPrimes.contains(INDEX)) {
+                    image[x][y] = WHITE_RGB;
+                } else {
+                    if (knownComposites.contains(INDEX)) {
+                        image[x][y] = uniqueColors.get(INDEX);
+                        usedColors.add(INDEX);
+                    } else {
+                        boolean isPrime = prime.checkIsPrime(BigInteger.valueOf(INDEX));
+                        if (isPrime) {
+                            knownPrimes.add(INDEX);
+                            image[x][y] = WHITE_RGB;
+                        } else {
+                            knownComposites.add(INDEX);
+                            image[x][y] = uniqueColors.get(INDEX);
+                            usedColors.add(INDEX);
+                        }
+                    }
+                }
+                INDEX++;
+            }
+        }
     }
-  }
 
-  protected BufferedImage getPixelMultiplied() {
-    int xIndex = 0;
-    int yIndex = 0;
-    for (int y = 0; y < height * pixelMultiplier; y++) {
-      for (int x = 0; x < width * pixelMultiplier; x++) {
-        if (y % pixelMultiplier == 0 && y != 0 && x == 0) {
-          yIndex++;
+    protected BufferedImage getPixelMultiplied() {
+        int xIndex = 0;
+        int yIndex = 0;
+        for (int y = 0; y < height * pixelMultiplier; y++) {
+            for (int x = 0; x < width * pixelMultiplier; x++) {
+                if (y % pixelMultiplier == 0 && y != 0 && x == 0) {
+                    yIndex++;
+                }
+                if (x % pixelMultiplier == 0 && x != 0) {
+                    xIndex++;
+                }
+                bufferedImage.setRGB(x, y, image[xIndex][yIndex]);
+            }
+            xIndex = 0;
         }
-        if (x % pixelMultiplier == 0 && x != 0) {
-          xIndex++;
-        }
-        bufferedImage.setRGB(x, y, image[xIndex][yIndex]);
-      }
-      xIndex = 0;
+        return bufferedImage;
     }
-    return bufferedImage;
-  }
 
-  void setPixelMultiplier(int pixelMultiplier) {
-    this.pixelMultiplier = pixelMultiplier;
-    bufferedImage =
-        new BufferedImage(
-            (int) width * pixelMultiplier,
-            (int) height * pixelMultiplier,
-            BufferedImage.TYPE_INT_RGB);
-  }
+    void setPixelMultiplier(int pixelMultiplier) {
+        this.pixelMultiplier = pixelMultiplier;
+        bufferedImage =
+                new BufferedImage(
+                        (int) width * pixelMultiplier,
+                        (int) height * pixelMultiplier,
+                        BufferedImage.TYPE_INT_RGB);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,60 +10,62 @@ import java.util.Date;
 import java.util.List;
 
 public class Main {
+    public static BufferedImage joinBufferedImage(BufferedImage img1,
+                                                  BufferedImage img2) {
+        int offset = 0;
+        int width = img1.getWidth() + img2.getWidth() + offset;
+        int height = Math.max(img1.getHeight(), img2.getHeight());
+        BufferedImage newImage = new BufferedImage(width, height,
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = newImage.createGraphics();
+        Color oldColor = g2.getColor();
+        g2.setPaint(Color.BLACK);
+        g2.fillRect(0, 0, width, height);
+        g2.setColor(oldColor);
+        g2.drawImage(img1, null, 0, 0);
+        g2.drawImage(img2, null, img1.getWidth() + offset, 0);
+        g2.dispose();
+        return newImage;
+    }
 
-  public static BufferedImage joinBufferedImage(BufferedImage img1,
-                                                BufferedImage img2) {
-    int offset = 50;
-    int width = img1.getWidth() + img2.getWidth() + offset;
-    int height = Math.max(img1.getHeight(), img2.getHeight());
-    BufferedImage newImage = new BufferedImage(width, height,
-            BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g2 = newImage.createGraphics();
-    Color oldColor = g2.getColor();
-    g2.setPaint(Color.BLACK);
-    g2.fillRect(0, 0, width, height);
-    g2.setColor(oldColor);
-    g2.drawImage(img1, null, 0, 0);
-    g2.drawImage(img2, null, img1.getWidth() + offset, 0);
-    g2.dispose();
-    return newImage;
-  }
-  public static void main(String[] args) throws Exception {
-    System.out.println("Time: " + LocalDateTime.now());
-    int width = 10;
-    int maxR = (int) (Math.random() * 200);
-    int maxG = (int) (Math.random() * 48);
-    int maxB = (int) (Math.random() * 200);
-    List<BufferedImage> imageList = new ArrayList<>();
-    List<Integer> knownPrimes = new ArrayList<>();
-    List<Integer> knownComposites = new ArrayList<>();
-    for (int w = width; w <= 50; w += 10) {
-      System.out.println("R-" + maxR + " G-" + maxG + " B-" + maxB);
-      System.out.println("required unique colors: " + (w * w * 5));
-      System.out.println("Number of possible colors: " + (maxR * maxG * maxB));
-      if ((w * w * 5) > (maxR * maxG * maxB)) {
-        throw new Exception("Too many required unique colors for RGB values.");
-      }
-      ArtImage artImage = new ArtImage(w, w * 5, maxR, maxG, maxB, knownPrimes, knownComposites);
-      artImage.setPixelMultiplier(1200 / w);
-      artImage.generateImage();
-      try {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Program starting! Time: " + LocalDateTime.now());
+        int width = 10;
+        int maxR = (int) (Math.random() * 255);
+        int maxG = (int) (Math.random() * 255);
+        int maxB = (int) (Math.random() * 255);
+        List<BufferedImage> imageList = new ArrayList<>();
+        List<Integer> knownPrimes = new ArrayList<>();
+        List<Integer> knownComposites = new ArrayList<>();
+        for (int w = width; w <= 50; w += 10) {
+            System.out.println("R-" + maxR + " G-" + maxG + " B-" + maxB);
+            System.out.println("required unique colors: " + (w * w * 5));
+            System.out.println("Number of possible colors: " + (maxR * maxG * maxB));
+            if ((w * w * 5) > (maxR * maxG * maxB)) {
+                throw new Exception("Too many required unique colors for RGB values.");
+            }
+            ArtImage artImage = new ArtImage(w, w * 5, maxR, maxG, maxB, knownPrimes, knownComposites);
+            artImage.setPixelMultiplier(1800 / w);
+            artImage.generateImage();
+            try {
+                String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+                BufferedImage image = artImage.getPixelMultiplied();
+                imageList.add(image);
+                String fileName = "artImages/ArtImage_" + w + "_x_" + w * 5 + "_" + timeStamp + "r-" + maxR + "_g-" + maxG + "_b-" + maxB + ".png";
+                ImageIO.write(image, "png",
+                        new File(fileName));
+                System.out.println("Time: " + LocalDateTime.now() + " Filename: " + fileName);
+            } catch (IOException e) {
+                System.out.println("Error: " + e);
+            }
+        }
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-        BufferedImage image = artImage.getPixelMultiplied();
-        imageList.add(image);
-        ImageIO.write(image, "png",
-                new File("artImages/ArtImage_" + w + "_x_" + w * 5 + "_" + timeStamp + "r-" + maxR + "_g-" + maxG + "_b-" + maxB + ".png"));
-        System.out.println("Time: " + LocalDateTime.now());
-      } catch (IOException e) {
-        System.out.println("Error: " + e);
-      }
+        BufferedImage joined = joinBufferedImage(imageList.get(0), imageList.get(1));
+        for (int i = 2; i < imageList.size(); i++) {
+            joined = joinBufferedImage(joined, imageList.get(i));
+        }
+        ImageIO.write(joined, "png",
+                new File("artImages/ArtImage_JOINED_" + timeStamp + "r-" + maxR + "_g-" + maxG + "_b-" + maxB + ".png"));
+        System.out.println("Program finished! Time: " + LocalDateTime.now());
     }
-    String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-    BufferedImage joined = joinBufferedImage(imageList.get(0), imageList.get(1));
-    for (int i = 2; i < imageList.size(); i++) {
-      joined = joinBufferedImage(joined, imageList.get(i));
-    }
-    ImageIO.write(joined, "png",
-            new File("artImages/ArtImage_JOINED_" + timeStamp + "r-" + maxR + "_g-" + maxG + "_b-" + maxB + ".png"));
-  }
 }
